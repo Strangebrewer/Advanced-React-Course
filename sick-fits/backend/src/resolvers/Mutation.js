ad26ea2e153ec5b4ce8584014585cb9b283ddd3e
Strangebrewer <BKAShambala@gmail.com>
2018-10-26 13:14:00 -0600

    done through video 24

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
+// These files ("resolvers") seem to be the same or similar to a controller.
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -31,6 +36,30 @@ const Mutations = {
     // TODO
     // delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    // lowercase the email to make signins easier later.
+    args.email = args.email.toLowerCase();
+    // has the password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the db
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+    // create JWT token for the user
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // this makes it a one-year cookie
+    });
+    // and then return the user to the browser
+    return user;
   }
 };
 
